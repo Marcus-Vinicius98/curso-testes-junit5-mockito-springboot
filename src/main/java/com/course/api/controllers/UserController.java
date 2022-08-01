6f1ce4446573b7eaeff109b7709203cb10cc7441
Marcus Vinicius <marcusviniferrer@gmail.com>
2022-08-01 16:42:17 -0300

    retornando dto para user resource

--- src/main/java/com/course/api/controllers/UserController.java
+++ src/main/java/com/course/api/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.course.api.controllers;
 
 import com.course.api.domain.User;
+import com.course.api.domain.dto.UserDTO;
 import com.course.api.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +18,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @GetMapping("/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
-       return ResponseEntity.ok().body(userService.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+       return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
 
 
     }
